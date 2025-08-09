629e8eba5ce18f92027892675834273d0b234abd
Axellrn13 <axellarno@gmail.com>
2025-08-09 20:41:11 +0200

    change tab name + add icon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,6 +21,10 @@ const inter = Inter({
 export default function App({ Component, pageProps }) {
   return (
     <div className={`${inter.className} min-h-screen bg-[#161616] text-white`}>
+      <Head>
+        <title>ScholarMind</title>
+        <link rel="icon" type="image/png" href="/logo.png" />
+      </Head>
       <Navbar />
       <main className="page-app">
         <Component {...pageProps} />
